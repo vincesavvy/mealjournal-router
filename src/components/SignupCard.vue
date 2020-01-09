570a39e8071a2a2e738061331ec36c7835979a00
vincesavvy <vsavard@realtermenergy.com>
2020-01-09 13:20:59 -0500

    Update SignupCard.vue
    Designed SignupCard and programmed it.

--- src/components/SignupCard.vue
+++ src/components/SignupCard.vue
@@ -1,32 +1,96 @@
 <template>
   <v-card class="mx-auto" max-width="400">
-    <v-img class="white--text align-end" height="200px" src="@/assets/signup.png"></v-img>
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="@/assets/signup.png"
+    ></v-img>
 
     <v-card-text class="text--primary">
-      <div>Whitehaven Beach</div>
+      <p style="text-align:center">{{ $t("line1") }}</p>
 
-      <div>Whitsunday Island, Whitsunday Islands</div>
+      <p>{{ $t("line2") }}</p>
     </v-card-text>
+    <!-- FORM -->
+    <v-form v-model="valid">
+      <!-- email -->
+      <v-container>
+        <v-text-field v-model="email" :rules="emailRules" required>
+          <template v-slot:label>{{ $t("line3") }}</template>
+        </v-text-field>
 
-    <v-card-actions>
-      <v-btn color="orange" text>{{ $t("message") }}</v-btn>
+        <!-- name -->
+        <v-text-field v-model="name" :rules="nameRules" required>
+          <template v-slot:label>{{ $t("line4") }}</template>
+        </v-text-field>
+        <br />
+        <!-- password -->
+        <v-text-field v-model="password" :rules="passwordRules" required>
+          <template v-slot:label>{{ $t("line5") }}</template>
+        </v-text-field>
 
-      <v-btn color="orange" text>Explore</v-btn>
-    </v-card-actions>
+        <!-- password confirmation -->
+        <v-text-field
+          v-model="confirmPassword"
+          :rules="confirmPasswordRules"
+          required
+        >
+          <template v-slot:label>{{ $t("line6") }}</template>
+        </v-text-field>
+
+        <!-- button -->
+        <v-row>
+          <v-card-actions>
+            <v-btn color="orange" outlined>{{ $t("validate button") }}</v-btn>
+          </v-card-actions>
+        </v-row>
+      </v-container>
+    </v-form>
   </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    valid: false,
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+
+    // functions
+    emailRules: [
+      v => !!v || "Required / Requis",
+      v => /.+@.+/.test(v) || "E-mail must be valid"
+    ],
+    nameRules: [v => !!v || "Required / Requis"],
+    passwordRules: [v => !!v || "Required / Requis"],
+    confirmPasswordRules: [v => !!v || "Required / Requis"]
+  })
+};
 </script>
 
 <i18n>
 {
     "en": {
-        "message":"hi this is bacon"
+        "validate button":"Submit",
+        "line1":"Instead of using a Google or Facebook account, you are free to register with your email.",
+        "line2":"Just fill up this form.",
+        "line3": "Email",
+        "line4": "Name",
+        "line5": "Password",
+        "line6": "Confirm password"
     },
     "fr": {
-        "message":"Salut, cest le message."
+        "validate button":"Soumettre",
+        "line1":"Au lieu de vous connectez à l'aide d'un compte Google ou Facebook, vous pouvez vous enregistrer avec votre courriel.",
+        "line2":"Vous n'avez qu'à remplir ce formulaire.",
+        "line3":"Courriel",
+        "line4":"Nom",
+        "line5":"Mot de passe",
+        "line6":"Confirmation du mot de passe"
+
+
     }
 }
 </i18n>
