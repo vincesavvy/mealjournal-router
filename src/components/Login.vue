c9dbbfa83f25dda867b1d6d4bc8af770266cb01c
vincesavvy <vsavard@realtermenergy.com>
2020-01-09 14:23:36 -0500

    Updated login.vue to add the new user sign-up possible.
    will need to route it now.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,13 @@
 <template>
   <!-- card -->
   <v-card class="mx-auto" max-width="400">
-    <v-img
-      class="white--text align-end"
-      height="200px"
-      contain
-      src="@/assets/login-image.png"
-    ></v-img>
+    <v-img class="white--text align-end" height="200px" contain src="@/assets/login-image.png"></v-img>
 
     <v-card-text class="text--primary">
       <div style="text-align:center">{{ $t("line1") }}</div>
       <br />
       <div style="text-align:center">{{ $t("line2") }}</div>
+      <br />
     </v-card-text>
     <hr style="margin:1em" />
 
@@ -44,26 +40,35 @@
                 <template v-slot:label>{{ $t("line4") }}</template>
               </v-text-field>
 
-              <v-text-field
-                v-model="password"
-                :rules="passwordRules"
-                required
-                type="password"
-              >
+              <v-text-field v-model="password" :rules="passwordRules" required type="password">
                 <template v-slot:label>{{ $t("line5") }}</template>
               </v-text-field>
 
               <div align="center">
-                <v-btn
-                  :disabled="!valid"
-                  color="success"
-                  class="mr-4"
-                  @click="validate"
-                  >Validate</v-btn
-                >
+                <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
               </div>
             </v-form>
           </v-row>
+          <br />
+          <!-- Signup button -->
+          <v-container>
+            <v-toolbar flat dense>
+              <v-toolbar-items>
+                <v-btn text small rounded color="primary">
+                  {{
+                  $t("line6")
+                  }}
+                </v-btn>
+                <!-- line -->
+                <v-divider class="mx-4" :inset="inset" vertical></v-divider>
+                <v-btn text small color="primary">
+                  {{
+                  $t("line7")
+                  }}
+                </v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+          </v-container>
         </v-container>
       </v-col>
     </v-row>
@@ -119,14 +124,19 @@ export default {
     "line2": "Use a Google or Facebook account, or use your email.",
     "line3": "email login:",
     "line4": "Email",
-    "line5": "Password"
+    "line5": "Password",
+    "line6": "new user sign-up",
+    "line7": "Sign up!"
   },
   "fr": {
     "line1": "Afin d'utiliser l'application MealJournal, vous devrez vous connecter à votre compte.",
     "line2": "Utilisez un compte Google ou Facebook, sinon connectez-vous avec votre courriel.",
     "line3": "Connection à l'aide du courriel:",
     "line4": "Courriel:",
-    "line5": "Mot de passe"
+    "line5": "Mot de passe",
+    "line6": "Pas de compte?",
+    "line7": "Creez un compte!"
+
 
 
   }
