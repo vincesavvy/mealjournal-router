44fd1a831912d95d2c3737c3f49ee9428f4c3e67
vincesavvy <vsavard@realtermenergy.com>
2020-01-13 11:13:17 -0500

    Made the navbar as a single component AND made it responsive.
    Conditional:
    if => Medium screens and up,
    else => small screens

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,7 @@
 <template>
   <v-app light>
     <!-- APP BAR -->
-    <v-app-bar app color="primary">
-      <div class="d-flex align-center">
-        <v-avatar size="90">
-          <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="@/assets/logo-2.png"
-          />
-        </v-avatar>
-        <v-card-title class="white--text display-1">MealJournal</v-card-title>
-        <div style="margin-right:15px">testing:</div>
-        <v-btn to="/" style="margin-right:15px">to home</v-btn>
-        <v-btn to="/Landing" style="margin-right:15px">to landing</v-btn>
-        <v-btn to="/signup" style="margin-right:15px">to signup</v-btn>
-      </div>
-
-      <v-spacer></v-spacer>
-      <div v-if="user" class="mr-2">
-        {{ user.name }}
-        <v-avatar color="indigo" size="35">
-          <v-img :src="user.photo" />
-        </v-avatar>
-      </div>
-
-      <div v-else class="mr-2">
-        NEED A LINE 118n
-        <v-avatar color="indigo" size="35">
-          <v-img src="@/assets/default-avatar.png" />
-        </v-avatar>
-      </div>
-
-      <!-- <v-img  src="@/assets/default-avatar.png" /> -->
-
-      <v-btn
-        rounded
-        outlined
-        x-small
-        color="white"
-        class="mr-2 font-italic text-capitalize"
-        @click="english"
-        >En</v-btn
-      >
-      <v-btn
-        rounded
-        outlined
-        x-small
-        color="white"
-        @click="french"
-        class="font-italic text-capitalize mr-2"
-        >fr</v-btn
-      >
-    </v-app-bar>
+    <Navbar />
     <!-- ROUTER INTEGRATION -->
     <v-content>
       <router-view />
@@ -62,7 +10,12 @@
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+
 export default {
+  components: {
+    Navbar
+  },
   name: "App",
   data() {
     return {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <!-- Large screens -->
+    <template v-if="$vuetify.breakpoint.mdAndUp">
+      <v-app-bar app color="primary">
+        <div class="d-flex align-center">
+          <v-avatar size="90">
+            <v-img
+              alt="Vuetify Logo"
+              class="shrink mr-2"
+              contain
+              src="@/assets/logo-2.png"
+            />
+          </v-avatar>
+          <v-card-title class="white--text display-1">MealJournal</v-card-title>
+          <div>
+            <div style="margin-right:15px">testing:</div>
+            <v-btn to="/" style="margin-right:15px">to home</v-btn>
+            <v-btn to="/Landing" style="margin-right:15px">to landing</v-btn>
+            <v-btn to="/signup" style="margin-right:15px">to signup</v-btn>
+          </div>
+        </div>
+
+        <v-spacer></v-spacer>
+        <div v-if="user" class="mr-2">
+          {{ user.name }}
+          <v-avatar color="indigo" size="35">
+            <v-img :src="user.photo" />
+          </v-avatar>
+        </div>
+
+        <div v-else class="mr-2">
+          NEED A LINE 118n
+          <v-avatar color="indigo" size="35">
+            <v-img src="@/assets/default-avatar.png" />
+          </v-avatar>
+        </div>
+
+        <!-- <v-img  src="@/assets/default-avatar.png" /> -->
+
+        <v-btn
+          rounded
+          outlined
+          x-small
+          color="white"
+          class="mr-2 font-italic text-capitalize"
+          @click="english"
+          >En</v-btn
+        >
+        <v-btn
+          rounded
+          outlined
+          x-small
+          color="white"
+          @click="french"
+          class="font-italic text-capitalize mr-2"
+          >fr</v-btn
+        >
+      </v-app-bar>
+    </template>
+
+    <!-- *** -->
+    <!-- Small screens -->
+    <!-- *** -->
+    <template v-else>
+      <v-app-bar app color="primary">
+        <div class="d-flex align-center">
+          <v-avatar size="90">
+            <v-img
+              alt="Meal Logo"
+              class="shrink mr-2"
+              contain
+              src="@/assets/logo-2.png"
+            />
+          </v-avatar>
+          <v-card-title class="white--text xs">MealJournal</v-card-title>
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <!-- <v-img  src="@/assets/default-avatar.png" /> -->
+
+        <v-btn
+          rounded
+          outlined
+          x-small
+          color="white"
+          class="mr-2 font-italic text-capitalize"
+          @click="english"
+          >En</v-btn
+        >
+        <v-btn
+          rounded
+          outlined
+          x-small
+          color="white"
+          @click="french"
+          class="font-italic text-capitalize mr-2"
+          >fr</v-btn
+        >
+      </v-app-bar>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      userLoggedIn: true,
+      userHasAvatar: false,
+      langs: ["en", "fr"]
+    };
+  },
+
+  methods: {
+    english() {
+      this.$i18n.locale = "en";
+    },
+    french() {
+      this.$i18n.locale = "fr";
+    }
+  },
+  created() {
+    this.$store.dispatch("getUserInfo");
+  },
+  computed: {
+    user() {
+      return this.$store.getters.profileInfo;
+    }
+  }
+};
+</script>
