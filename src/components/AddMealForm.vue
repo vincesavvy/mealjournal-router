4b904590994f7fd9e442d5cb1a3cfed5e56513cc
vincesavvy <vsavard@realtermenergy.com>
2020-01-14 11:55:05 -0500

    Updated the form so it is now dynamic, complete and bilingual*.
    Bilingual: because of some limitations in vue.js and vuetify, I could not render everything in i18n... it works fine, but can look a bit weird.

--- src/components/AddMealForm.vue
+++ src/components/AddMealForm.vue
@@ -2,6 +2,8 @@
   <div>
     <template>
       <v-container>
+        <!-- Show form -->
+        <v-btn @click="showForm">show form</v-btn>
         {{ $t("line0.1") }}
         <hr />
         <div style="margin-top: 15px">{{ $t("line0.2") }}</div>
@@ -11,12 +13,15 @@
         <div v-if="$vuetify.breakpoint.mdAndUp">
           <v-row>
             <v-col cols="6">
-              <v-date-picker v-model="pickerDate" width="290"></v-date-picker>
+              <v-date-picker
+                v-model="form.pickerDate"
+                width="290"
+              ></v-date-picker>
             </v-col>
             <!-- Time Picker -->
             <v-col cols="6">
               <v-time-picker
-                v-model="pickerTime"
+                v-model="form.pickerTime"
                 color="green lighten-1"
                 header-color="primary"
                 width="278"
@@ -29,12 +34,15 @@
         <div v-else>
           <v-row>
             <v-col cols="6">
-              <v-date-picker v-model="pickerDate" width="200"></v-date-picker>
+              <v-date-picker
+                v-model="form.pickerDate"
+                width="200"
+              ></v-date-picker>
             </v-col>
             <!-- Time Picker -->
             <v-col cols="6">
               <v-time-picker
-                v-model="pickerTime"
+                v-model="form.pickerTime"
                 color="green lighten-1"
                 header-color="primary"
                 width="220"
@@ -51,7 +59,7 @@
             <v-row>
               <v-col>
                 <v-text-field
-                  v-model="mealName"
+                  v-model="form.mealName"
                   :rules="mealNameRules"
                   required
                 >
@@ -63,7 +71,7 @@
             <!-- Place Field -->
             <v-row>
               <v-col>
-                <v-text-field v-model="place">
+                <v-text-field v-model="form.place">
                   <template v-slot:label>{{ $t("line104") }}</template>
                 </v-text-field>
               </v-col>
@@ -72,7 +80,7 @@
             <!-- With Whom Field -->
             <v-row>
               <v-col>
-                <v-text-field v-model="withWhom">
+                <v-text-field v-model="form.withWhom">
                   <template v-slot:label>{{ $t("line105") }}</template>
                 </v-text-field>
               </v-col>
@@ -83,20 +91,20 @@
             <v-row>
               <v-col>
                 <v-container fluid>
-                  <v-radio-group v-model="hunger" row>
-                    <v-radio value="radio-1">
+                  <v-radio-group v-model="form.hunger" row>
+                    <v-radio value="Not at all / Pas du tout">
                       <template v-slot:label>{{ $t("line112") }}</template>
                     </v-radio>
 
-                    <v-radio value="radio-2">
+                    <v-radio value="Somewhat hungry / Un peu faim">
                       <template v-slot:label>{{ $t("line113") }}</template>
                     </v-radio>
 
-                    <v-radio value="radio-3">
+                    <v-radio value="Hungry / Faim normale">
                       <template v-slot:label>{{ $t("line114") }}</template>
                     </v-radio>
 
-                    <v-radio value="radio-4">
+                    <v-radio value="Really hungry / Très faim">
                       <template v-slot:label>{{ $t("line115") }}</template>
                     </v-radio>
                   </v-radio-group>
@@ -107,7 +115,7 @@
             <!-- Mood Field -->
             <v-row>
               <v-col>
-                <v-text-field v-model="mood">
+                <v-text-field v-model="form.mood">
                   <template v-slot:label>{{ $t("line107") }}</template>
                 </v-text-field>
               </v-col>
@@ -118,20 +126,20 @@
             <v-row>
               <v-col>
                 <v-container fluid>
-                  <v-radio-group v-model="fullness" row>
-                    <v-radio value="radio-1">
+                  <v-radio-group v-model="form.fullness" row>
+                    <v-radio value="Not at all / Pas du tout">
                       <template v-slot:label>{{ $t("line108") }}</template>
                     </v-radio>
 
-                    <v-radio value="radio-2">
+                    <v-radio value="Somewhat full / Un peu rassasié">
                       <template v-slot:label>{{ $t("line109") }}</template>
                     </v-radio>
 
-                    <v-radio value="radio-3">
+                    <v-radio value="Full / Plein">
                       <template v-slot:label>{{ $t("line110") }}</template>
                     </v-radio>
 
-                    <v-radio value="radio-4">
+                    <v-radio value="I overate / Trop mangé">
                       <template v-slot:label>{{ $t("line111") }}</template>
                     </v-radio>
                   </v-radio-group>
@@ -143,7 +151,7 @@
             <v-row>
               <v-col>
                 <v-select
-                  v-model="value"
+                  v-model="form.protein"
                   :items="proteinOptions"
                   attach
                   multiple
@@ -153,12 +161,15 @@
                 </v-select>
                 <v-row>
                   <v-col cols="2">
-                    <v-checkbox v-model="checkbox1">
+                    <v-checkbox v-model="checkboxProtein">
                       <template v-slot:label>{{ $t("line119") }}</template>
                     </v-checkbox>
                   </v-col>
                   <v-col cols="10">
-                    <v-text-field v-if="checkbox1" v-model="proteinsOther">
+                    <v-text-field
+                      v-if="checkboxProtein"
+                      v-model="form.proteinOther"
+                    >
                       <template v-slot:label>{{ $t("line120") }}</template>
                     </v-text-field>
                   </v-col>
@@ -166,39 +177,79 @@
               </v-col>
             </v-row>
 
-            <!-- div if other is true then text field -->
-
             <!-- Vegetable Field -->
             <v-row>
               <v-col>
-                <v-text-field v-model="veggies">
+                <v-select
+                  v-model="form.veggies"
+                  :items="veggiesOptions"
+                  attach
+                  multiple
+                  chips
+                >
                   <template v-slot:label>{{ $t("line3") }}</template>
-                </v-text-field>
+                </v-select>
+                <v-row>
+                  <v-col cols="2">
+                    <v-checkbox v-model="checkboxVeggies">
+                      <template v-slot:label>{{ $t("line119") }}</template>
+                    </v-checkbox>
+                  </v-col>
+                  <v-col cols="10">
+                    <v-text-field
+                      v-if="checkboxVeggies"
+                      v-model="form.veggiesOther"
+                    >
+                      <template v-slot:label>{{ $t("line121") }}</template>
+                    </v-text-field>
+                  </v-col>
+                </v-row>
               </v-col>
             </v-row>
 
             <!-- Carbs Field -->
             <v-row>
               <v-col>
-                <v-text-field v-model="carbs">
+                <v-select
+                  v-model="form.carbs"
+                  :items="carbsOptions"
+                  attach
+                  multiple
+                  chips
+                >
                   <template v-slot:label>{{ $t("line4") }}</template>
-                </v-text-field>
+                </v-select>
+                <v-row>
+                  <v-col cols="2">
+                    <v-checkbox v-model="checkboxCarbs">
+                      <template v-slot:label>{{ $t("line119") }}</template>
+                    </v-checkbox>
+                  </v-col>
+                  <v-col cols="10">
+                    <v-text-field
+                      v-if="checkboxCarbs"
+                      v-model="form.carbsOther"
+                    >
+                      <template v-slot:label>{{ $t("line122") }}</template>
+                    </v-text-field>
+                  </v-col>
+                </v-row>
               </v-col>
             </v-row>
 
             <!-- reason Field -->
             <v-row>
               <v-col>
-                <v-text-field v-model="reason">
+                <v-text-field v-model="form.reason">
                   <template v-slot:label>{{ $t("line116") }}</template>
                 </v-text-field>
               </v-col>
             </v-row>
 
-            <!-- reason Field -->
+            <!-- Mood after Field -->
             <v-row>
               <v-col>
-                <v-text-field v-model="moodAfter">
+                <v-text-field v-model="form.moodAfter">
                   <template v-slot:label>{{ $t("line117") }}</template>
                 </v-text-field>
               </v-col>
@@ -207,7 +258,6 @@
         </v-form>
       </v-container>
     </template>
-    <!-- <template v-else>hi</template> -->
   </div>
 </template>
 
@@ -217,18 +267,39 @@ export default {
     return {
       value: null,
 
-      checkbox1: false,
+      checkboxProtein: false,
+      checkboxVeggies: false,
+      checkboxCarbs: false,
 
       proteinsOther: "",
 
       proteinOptions: [
-        "chicken",
-        "fish",
-        "dairy",
-        "beans",
-        "pork",
-        "beef",
+        "chicken / poulet",
+        "fish / poisson",
+        "dairy / produits laitiers",
+        "beans / légumineuses",
+        "pork / porc",
+        "beef / boeuf",
         "tofu"
+      ],
+
+      veggiesOptions: [
+        "carrot / carotte",
+        "broccoli / brocoli",
+        "cabbage / chou",
+        "celery / céleri",
+        "lettuce / laitue",
+        "onions / oignons",
+        "bell peppers / poivrons",
+        "tomatoes / tomates"
+      ],
+
+      carbsOptions: [
+        "bread / pain",
+        "pastas / pâtes",
+        "potatoes / pommes de terre",
+        "quinoa",
+        "oats / avoine"
       ],
 
       // date picker
@@ -239,28 +310,34 @@ export default {
       mealName: "",
       mealNameRules: [v => !!v || "Required / Requis"],
 
-      place: "",
-
-      withWhom: "",
-
-      mood: "",
-
-      hunger: null,
-
-      fullness: null,
-
-      protein: "",
-
-      veggies: "",
-
-      carbs: "",
-
-      reason: "",
-
-      moodAfter: ""
+      form: {
+        pickerDate: new Date().toISOString().substr(0, 10),
+        pickerTime: null,
+        mealName: null,
+        place: null,
+        withWhom: null,
+        mood: null,
+        hunger: null,
+        fullness: null,
+        protein: null,
+        proteinOther: null,
+        veggies: null,
+        veggiesOther: null,
+        carbs: null,
+        carbsOther: null,
+        reason: null,
+        moodAfter: null
+      }
     };
+  },
+  methods: {
+    /* eslint-disable no-alert, no-console */
+    showForm() {
+      console.log(this.form);
+    }
   }
 };
+
 /* Use Dictionary with: {{ $t("mealName") }} */
 </script>
 
@@ -271,8 +348,8 @@ export default {
     "line0.2": "Time of meal:",
     "line1": "Meal's name (Example: spaghetti, sandwich, rice and chicken...)",
     "line2": "Protein sources",
-    "line3": "Vegetable source (Example: carrots, tomatoes, cabbage, salad, corn, cucumber...)",
-    "line4": "Carbohydrates source (Example: bread, pastas, potatoes, quinoa, oats...)",
+    "line3": "Vegetable sources",
+    "line4": "Carbohydrates sources",
     "line104": "Place where the meal was consumed (Example: kitchen, living room, bedroom...)",
     "line105": "With whom was the meal consumed (Example: husband, wife, mother, alone...)",
     "line106": "Level of hunger:",
@@ -289,16 +366,18 @@ export default {
     "line117": "Mood after eating (Example: grumpy, fine, excited...)",
     "line118": "Level of fullness:",
     "line119": "Other",
-    "line120": "Other source of proteins"
+    "line120": "Other source of proteins",
+    "line121": "Other source of vegetables",
+    "line122": "Other source of carbohydrates"
 
   },
   "fr": {
     "line0.1": "Ajouter un repas",
     "line0.2": "Date et heure du repas:",
     "line1": "Nom du repas (Exemple: spaghetti, sandwich, riz au poulet...) ",
-    "line2": "Source de protéines (Exemple: poulet, poisson, produits laitiers, légumineuse, porc, boeuf, tofu...)",
-    "line3": "Source de légumes (Exemple: carottes, tomates, chou, salade, maïs, concombre...)",
-    "line4": "Source de féculents (Exemple: pain, pâtes, pommes de terres, quinoa, avoine...)",
+    "line2": "Sources de protéines",
+    "line3": "Sources de légumes",
+    "line4": "Sources de féculents",
     "line104": "Endroit où le repas à été mangé (Exemple: cuisine, salon, chambre...)",
     "line105": "Avec qui le repas à-t-il été mangé (Exemple: Époux, mère, seul...)",
     "line106": "Niveau de faim:",
@@ -315,7 +394,9 @@ export default {
     "line117": "Humeur après avoir mangé (Exemple: marabout, bien, excité...)",
     "line118": "Niveau de satiété:",
     "line119": "Autre",
-    "line120": "Autre source de proteines"
+    "line120": "Autre source de proteines",
+    "line121": "Autre source de légumes",
+    "line122": "Autre source de féculents"
   }
 }
 </i18n>
